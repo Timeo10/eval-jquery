2018f2f6c0819e189143224453e68bb07a11cc3a
Timeo10 <timeo.ladan10@gmail.com>
2022-01-12 10:25:37 +0100

    first commit

--- B1_eval_jquery/B1_eval_jquery_files/assets/B1_eval_jquery.js
+++ B1_eval_jquery/B1_eval_jquery_files/assets/B1_eval_jquery.js
@@ -3,3 +3,27 @@
 Votre code ici
 
 -------------------------------------------------------------------------- */
+function switchRedSquare() {
+
+    $('.custom-control').on('click', function () {
+        $('.custom-control').addClass('is-active');
+        $(this).removeClass('is-active');
+
+
+    })
+
+
+}
+
+function controlRedSquare() {
+    $('.redsquare-controls').removeAttr("disabled", false);
+
+}
+
+
+function jumbotron() {
+
+   $('.jumbotron ul').addClass('list-group');
+   $(' .jumbotron li').addClass('list-groupe-item');
+
+}
